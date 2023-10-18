2cf1551f32e5e4559661aabfd5df0d2efeeb3b65
salman-41 <salman.yz@outlook.com>
2023-10-18 22:39:22 +0500

    photos added from api

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+
+let photosArray = [];
+
+//Create Elements for Links & Photos, Add to DOM
+function displayPhotos() {
+  photosArray.forEach((photo) => {
+    //Create <a> to link to Unsplash
+    const anchorItem = document.createElement("a");
+    anchorItem.setAttribute("href", photo.links.html);
+
+    //Create <img> for photos
+    const image = document.createElement("img");
+    image.setAttribute("src", photo.urls.regular);
+    image.setAttribute("alt", photo.alt_description);
+    image.setAttribute("title", photo.alt_description);
+
+    //Put <img> inside <a> then both inside ${imageContainer}
+    imageContainer.appendChild(anchorItem);
+    anchorItem.appendChild(image);
+  });
+}
+
 //Unsplash API
 const apiKey = "rXogb3nNf1uqzcKOnnp7dPDydt8fChP3rglMYADMuA4";
 const count = 10;
@@ -7,8 +31,9 @@ const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&coun
 async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
-    const data = await response.json;
-    console.log(data);
+    photosArray = await response.json;
+    console.log(photosArray);
+    displayPhotos();
   } catch (error) {
     //error
   }
